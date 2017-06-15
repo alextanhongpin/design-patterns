cb329fb2ba8f54cbc9db4cb7cf75954e85a14419
Hong Pin Alex Tan <alextan@administrators-MacBook-Pro.local>
2017-06-15 09:58:24 +0800

    Add decorator pattern

--- prototype/prototype.py
+++ prototype/prototype.py
@@ -34,18 +34,18 @@ class Prototype:
         obj.__dict__.update(attr)
         return obj
     
-    def main():
-        b1 = Book('The C Programming Language', ('Brian W. Kernighan', 'Dennis M.Ritchie'),
-        price=118, publisher='Prentice Hall', length=228, publication_date='1978-02-22', tags=('C', 'programming', 'algorithms','data structures'))
-        
-        prototype = Prototype()
-        cid = 'k&r-first'
-        prototype.register(cid, b1)
-        b2 = prototype.clone(cid, name='The C Programming Language (ANSI)', price=48.99, length=274, publication_date='1988-04-01', edition=2)
-        
-        for i in (b1,b2):
-            print(i)
-        print('ID b1: {} != ID b2 : {}'.format(id(b1), id(b2)))
+def main():
+    b1 = Book('The C Programming Language', ('Brian W. Kernighan', 'Dennis M.Ritchie'),
+    price=118, publisher='Prentice Hall', length=228, publication_date='1978-02-22', tags=('C', 'programming', 'algorithms','data structures'))
+    
+    prototype = Prototype()
+    cid = 'k&r-first'
+    prototype.register(cid, b1)
+    b2 = prototype.clone(cid, name='The C Programming Language (ANSI)', price=48.99, length=274, publication_date='1988-04-01', edition=2)
+    
+    for i in (b1,b2):
+        print(i)
+    print('ID b1: {} != ID b2 : {}'.format(id(b1), id(b2)))
 
 if __name__ == '__main__':
     main()
